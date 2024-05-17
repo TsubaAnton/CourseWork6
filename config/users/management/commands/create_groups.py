23e18d6bf7530f9ffaed56ffdda04ecc86fe6611
TsubaAnton <antontsuba2003@gmail.com>
2024-05-17 21:28:31 +0200

    CourseWork 6.3

--- config/users/management/commands/create_groups.py
+++ config/users/management/commands/create_groups.py
@@ -1,5 +1,6 @@
 from django.core.management import BaseCommand
 from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 
 class Command(BaseCommand):
@@ -8,22 +9,31 @@ class Command(BaseCommand):
             {'name': 'managers', 'description': 'Менеджеры'}
         ]
 
+        permissions = [
+            'add_client',
+            'change_client',
+            'delete_client',
+            'view_client',
+            'add_newsletter',
+            'change_newsletter',
+            'delete_newsletter',
+            'view_newsletter',
+        ]
+
         for group_data in groups:
             group, created = Group.objects.get_or_create(name=group_data['name'])
             if created:
                 group.description = group_data['description']
                 group.save()
 
-    permissions = [
-        'users.block_user',
-        'users.view_user_list',
-        'service.view_all_newsletter',
-        'service.deactivate_newsletter',
-        'blog.delete_blog',
-        'blog.change_blog',
-        'blog.add_blog',
-    ]
-    for perm in permissions:
-        app_label, codename = perm.split('.')
-        permission = Permission.objects.get(content_type__app_label=app_label, codename=codename)
-        group.permissions.add(permission)
+            for perm in permissions:
+                try:
+                    # Get permission object
+                    permission = Permission.objects.get(codename=perm)
+                    # Add permission to group
+                    group.permissions.add(permission)
+                except Permission.DoesNotExist:
+                    self.stdout.write(self.style.ERROR(f'Permission {perm} not found.'))
+
+            group.save()
+            self.stdout.write(self.style.SUCCESS(f'Group {group.name} with permissions created or updated.'))
